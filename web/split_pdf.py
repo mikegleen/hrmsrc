01df295ba17a4d34fd63249a4987ed38d49ccee1
Mike Gleen <mike.gleen@gmail.com>
2016-12-10 15:11:11 +0000

    Update to PyPDF2 and tidy.

--- web/split_pdf.py
+++ web/split_pdf.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-  needed because of embedded "£"
 """
-
+Input is a PDF file specified in the first parameter.
+Output is one file in the current directory per page in the input file. The
+name of the output files is <input basename>-page<n> starting with the zero-th
+page.
 """
-
-from pyPDF2 import PdfFileWriter, PdfFileReader
+import os
+from PyPDF2 import PdfFileWriter, PdfFileReader
 import sys
 
 
-def main():
+def main(infilename):
+    inputpdf = PdfFileReader(open(infilename, "rb"))
+    basename = os.path.splitext(infilename)[0]
     for i in range(inputpdf.numPages):
         output = PdfFileWriter()
         output.addPage(inputpdf.getPage(i))
-        with open("document-page{}.pdf".format(i), "wb") as outputStream:
+        with open("{}-page{}.pdf".format(basename, i), "wb") as outputStream:
             output.write(outputStream)
 
 if __name__ == '__main__':
     if sys.version_info.major < 3:
         raise ImportError('requires Python 3')
-    inputpdf = PdfFileReader(open("document.pdf", "rb"))
-    main()
+    main(sys.argv[1])
