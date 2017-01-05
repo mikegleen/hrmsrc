9d531785307d10579191d7bbc1eeea719405284f
Mike Gleen <mike.gleen@gmail.com>
2017-01-05 22:57:48 +0100

    Replace the logic that creates just two thumbnail files with logic that creates a list of files or takes the output format from parameters.

--- web/thumb.py
+++ web/thumb.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-  needed because of embedded "£"
 """
-Create two thumbnail images:
-    portrait: width 342, height 484
-    landscape: width 1280, height 826
+Create thumbnail images.
 
 See get_args for details.
 
@@ -16,15 +14,15 @@ import sys
 
 BACKGROUND = 'F2F4F6'
 
-ImgSize = namedtuple('ImgSize', ['w', 'h'])
+ImgSize = namedtuple('ImgSize', ['w', 'h', 'name'])
 
 THUMB_IMG_SIZES = {
-    'ev-p': ImgSize(342, 484),  # event portrait
-    'ev-l': ImgSize(1280, 826),  # event landscape
-    'm-t': ImgSize(1280, 826),  # media centre thumbnail
-    'n-t': ImgSize(1280, 826),  # news thumbnail
-    'n-w': ImgSize(1280, 410),  # news wide
-    'ex-t': ImgSize(700, 454),  # exhibition thumbnail
+    'ev-p': ImgSize(342, 484, 'event portrait'),
+    'ev-l': ImgSize(1280, 826, 'event landscape'),
+    'm-t': ImgSize(1280, 826, 'media centre thumbnail'),
+    'n-t': ImgSize(1280, 826, 'news thumbnail'),
+    'n-w': ImgSize(1280, 410, 'news wide'),
+    'ex-t': ImgSize(700, 454, 'exhibition thumbnail'),
 }
 
 
@@ -55,7 +53,7 @@ def pad_height(inimage, target_width, target_height):
           target_height)
     width, height = inimage.size
     wh_ratio = float(width) / float(height)
-    unpadded_height = int(target_width / wh_ratio)
+    unpadded_height = int(round(target_width / wh_ratio))
     trace(2, 'Resizing image to ({}, {})', target_width, unpadded_height)
     resized_image = inimage.resize((target_width, unpadded_height))
     y_origin = int(math.ceil((target_height - unpadded_height) / 2.))
@@ -82,7 +80,7 @@ def pad_width(inimage, target_width, target_height):
         trace(2, 'Overriding target width: ', target_width)
     width, height = inimage.size
     wh_ratio = float(width) / float(height)
-    unpadded_width = int(target_height * wh_ratio)
+    unpadded_width = int(round(target_height * wh_ratio))
     trace(2, "Resizing image to ({}, {})", unpadded_width, target_height)
     resized_image = inimage.resize((unpadded_width, target_height))
     x_origin = int(math.ceil((target_width - unpadded_width) / 2.))
@@ -96,79 +94,96 @@ def oneimage(img, sizeref):
     pass
 
 
-def onefile(infile, outdir):
+def onefile(infile, outdir, img_sizes):
     """
     Iterate over the target image sizes that we want to create and resize and
     pad them accordingly.
     """
     basename = os.path.basename(infile)  # 'a/b/xyz.jpg' -> 'xyz.jpg'
     front, extension = os.path.splitext(basename)  # 'xyz.jpg' -> 'xyz', '.jpg'
-    input_image = Image.open(infile)
+    try:
+        input_image = Image.open(infile)
+    except OSError:
+        print('Skipping unrecognized file:', infile)
+        return
     width, height = input_image.size
     wh_ratio = width / height
     trace(2, "Input: {}\nSize (width, height) in pixels: {}, {}, "
           "width/height = {:.3f}", infile, width, height, wh_ratio)
-    for thumb in THUMB_IMG_SIZES:
-        thumb_width, thumb_height = THUMB_IMG_SIZES[thumb]
-        thumb_wh_ratio = thumb_width / thumb_height
+    for thumb in img_sizes:
+        thumb_width, thumb_height, thumb_name = img_sizes[thumb]
+        thumb_wh_ratio = float(thumb_width) / float(thumb_height)
         if wh_ratio > thumb_wh_ratio:
             thumb_image = pad_height(input_image, thumb_width, thumb_height)
         else:
             thumb_image = pad_width(input_image, thumb_width, thumb_height)
-        thumb_name = front + '_' + thumb + '.jpeg'
-        thumb_path = os.path.join(outdir, thumb_name)
-        trace(1, 'Saving thumbnail: {}', thumb_path)
+        thumb_file_name = front + '_thumb_' + thumb + extension
+        thumb_path = os.path.join(outdir, thumb_file_name)
+        trace(1, 'Saving thumbnail: {}, ({})', thumb_path, thumb_name)
         thumb_image.save(thumb_path)
 
 
 def main(args):
+    wh = THUMB_IMG_SIZES
+    if args.width:
+        wh = {'th': (args.width, args.height, 'anonymous')}
     if os.path.isdir(args.infile):
         for filename in os.listdir(args.infile):
-            onefile(filename, args.infile)
+            if '_thumb_' in filename:
+                continue
+            onefile(filename, args.infile, wh)
     else:
-        onefile(args.infile, args.outdir)
+        onefile(args.infile, args.outdir, wh)
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='''
-    Create two thumbnail images:
-    portrait: (width 342, height 484)
-    landscape: (width 1280, height 826).
+    q = THUMB_IMG_SIZES
+    thumblist = ''.join(['\n    {}: {}x{} ({})'
+                        .format(x, q[x].w, q[x].h, q[x].name) for x in q])
+    parser = argparse.ArgumentParser(formatter_class=
+                                     argparse.RawDescriptionHelpFormatter,
+                                     description='''
+    Create multiple thumbnail images using a predefined set of dimensions or
+    width and height values passed as parameters.
 
     The thumbnail files are created with the same name as the original file
-    but with _thumb_portrait and _thumb_landscape appended before the
-    extension.
-    ''')
-    parser.add_argument('infile', help='''Original sized input file.''')
+    but with the designated abbreviation appended before the extension. Thus
+    "jellytots.jpeg" becomes "jellytots_thumb_ev-l.jpeg" for Event Landscape
+    thumbnail files. The default thumbnail files produced are:
+    ''' + thumblist)
+    parser.add_argument('infile', help='''There are two modes. If infile is
+    a simple file, it is processed and output files are written to outdir.
+    If it is a directory, all of the files in that directory are processed and
+    output is written to the same directory. To prevent output files being
+    re-processed, file names including "_thumb_" are skipped.''')
     parser.add_argument('-b', '--background', default=BACKGROUND, help='''
         Hex number describing the background color. Default = {}.
         The number should be coded as six hex digits. The leading "0x" is
-        optional.'''.format(BACKGROUND))
+        optional. If the number given is not valid hexadecimal, the program
+        will abort.'''.format(BACKGROUND))
     parser.add_argument('--height', type=int, default=0, help='''
-        Set an explicit height to pad to (sorry, -h is taken).
-        ''')
-    parser.add_argument('-l', '--landscape', action='store_true', help='''
-        If set, only produce a landscape thumbnail.
+        Set an explicit height to pad to (sorry, -h is taken). You must also
+        specify width. If specified, a single thumbnail file is created rather
+        than the set of files internally defined. An abbreviation of 'th' is
+        used for the output filename.
         ''')
     parser.add_argument('-o', '--outdir',
                         default=os.path.join('results', 'thumb'),
                         help='''Directory to contain the
         output landscape file. If omitted, the default is "results/thumb".
         ''')
-    parser.add_argument('-p', '--portrait', action='store_true', help='''
-        If set, only produce a portrait thumbnail.
-        ''')
     parser.add_argument('-v', '--verbose', type=int, default=1, help='''
         Set the verbosity. The default is 1 which prints summary information.
         ''')
     parser.add_argument('-w', '--width', type=int, default=0, help='''
-        Set an explicit width to pad to (portrait only).
+        Set an explicit width to pad to. You must also specify width.
         ''')
 
     args = parser.parse_args()
     args.background_tuple = make_background(args.background)
-    if args.landscape is False and args.portrait is False:
-        args.landscape = args.portrait = True
+    if bool(args.height) != bool(args.width):
+        print('You must specify either both width and height or neither.')
+        raise ValueError
     return args
 
 if __name__ == '__main__':
