b0e5011e69c41b7a8537bb9250c52e8be8de38ca
Mike Gleen <mike.gleen@gmail.com>
2017-04-22 07:37:21 +0100

    working version

--- web/split_pdf.py
+++ web/split_pdf.py
@@ -5,21 +5,32 @@ Output is one file in the current directory per page in the input file. The
 name of the output files is <input basename>-page<n> starting with the zero-th
 page.
 """
-import os
+import argparse
+import os.path
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import sys
 
 
-def main(infilename):
-    inputpdf = PdfFileReader(open(infilename, "rb"))
-    basename = os.path.splitext(infilename)[0]
+def getargs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile')
+    parser.add_argument('-o', '--outdir', default='.')
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    inputpdf = PdfFileReader(open(args.infile, "rb"))
+    basename = os.path.splitext(args.infile)[0]
     for i in range(inputpdf.numPages):
         output = PdfFileWriter()
         output.addPage(inputpdf.getPage(i))
-        with open("{}-page{}.pdf".format(basename, i), "wb") as outputStream:
+        outdirpath = os.path.join(
+            args.outdir, "{}-page{:03}.pdf".format(basename, i))
+        with open(outdirpath, "wb") as outputStream:
             output.write(outputStream)
 
 if __name__ == '__main__':
     if sys.version_info.major < 3:
         raise ImportError('requires Python 3')
-    main(sys.argv[1])
+    main(getargs())
