7e440c4ed5148aad364f10e0ac124ad1078e065e
Mike Gleen <mike.gleen@gmail.com>
2017-05-02 10:28:30 +0100

    Initial commit.

---
+++ tickets/admission_types.py
@@ -0,0 +1,14 @@
+admission_types = [
+    "Adult",
+    "Adult with Gift Aid",
+    "Child with Gift Aid",
+    "Child, 5 - 18",
+    "Concession",
+    "Concession with Gift Aid",
+    "Family",
+    "Family with Gift Aid",
+    "Free",
+    "Group",
+    "Over 65 with Gift Aid",
+    "Over 65s",
+]
