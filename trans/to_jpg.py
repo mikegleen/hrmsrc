530c5095c5c2008b486750197ea98ccc1ba46590
Mike Gleen <mike.gleen@gmail.com>
2018-04-25 12:01:09 +0100

    Add functionality to do single files and to specify the base directory for multiple files.

--- trans/to_jpg.py
+++ trans/to_jpg.py
@@ -2,6 +2,18 @@
 """
 Convert a PDF file to JPEG using the OSX utility sips.
 
+There are three modes:
+1.  Specify the input and output files.
+2.  Specify the directory containing the structure below.
+3.  If no parameters are given, the default directory is used.
+
+Directory structure:
+
+base
+    - pdf
+    - jpg
+    - archive
+
 The files must reside in directories with predefined names all within a
 predefined base directory (see below).
 
@@ -18,15 +30,35 @@ import os.path
 import re
 import shutil
 import subprocess
+import sys
 
 BASEDIR = '/users/mlg/play/hrm'
 SIPSCMD = 'sips -s format jpeg "{pdf_file}" --out "{jpg_file}"'
 VERBOS = 0
 
+
+def one_file(infile, outfile):
+    scmd = SIPSCMD.format(pdf_file=infile, jpg_file=outfile)
+    if VERBOS > 0:
+        print(scmd)
+    subprocess.check_call(scmd, shell=True)
+
+
 if __name__ == '__main__':
-    pdfbase = os.path.join(BASEDIR, 'pdf')
-    jpgbase = os.path.join(BASEDIR, 'jpg')
-    archivebase = os.path.join(BASEDIR, 'archive')
+    if len(sys.argv) > 2:
+        one_file(sys.argv[1], sys.argv[2])
+        sys.exit(0)
+    elif len(sys.argv) > 1:
+        basedir = sys.argv[1]
+        if not os.path.isdir(basedir):
+            print("Parameter must be a directory.\nNo action taken.")
+            sys.exit(1)
+    else:
+        basedir = BASEDIR
+
+    pdfbase = os.path.join(basedir, 'pdf')
+    jpgbase = os.path.join(basedir, 'jpg')
+    archivebase = os.path.join(basedir, 'archive')
     for pdfname in os.listdir(pdfbase):
         pdfpath = os.path.join(pdfbase, pdfname)
         if VERBOS > 0:
@@ -40,8 +72,5 @@ if __name__ == '__main__':
         jpegpath = os.path.join(jpgbase, jpegname)
         if VERBOS > 0:
             print(jpegpath)
-        scmd = SIPSCMD.format(pdf_file=pdfpath, jpg_file=jpegpath)
-        if VERBOS > 0:
-            print(scmd)
-        subprocess.check_call(scmd, shell=True)
+        one_file(pdfpath, jpegpath)
         shutil.move(pdfpath, archivebase)
